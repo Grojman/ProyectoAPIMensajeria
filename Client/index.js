91931a1f0254ce75a4189baf404c603a8e57d29b
JorgeGomis <jorge.gomis@iesdoctorbalmis.com>
2023-06-13 21:14:54 +0200

    Zona de inicio de sesión terminada

--- Client/index.js
+++ Client/index.js
@@ -1,13 +1,15 @@
 window.addEventListener("load", () => {
     let client = new WebSocket(url);
     document.querySelector("button").addEventListener("click", (event) => {
-        client.send(`${event.target.Id}/${document.querySelector("#name").Value}/${document.querySelector("#pass").Value}`)
+        //COMPROBAR QUE LOS CAMPOS NO ESTÁN VACÍOS
+        document.querySelector("#name").value && document.querySelector("#pass").value ? client.send(`${event.target.id}/${document.querySelector("#name").value}/${document.querySelector("#pass").value}`) : alert("Los campos no pueden estar vacíos")
+        event.preventDefault()
     })
     client.onmessage = (event) => {
         let data = JSON.parse(event.data)
         switch (data.Status) {
             case 0:
-                window.location.href = `mainPage/main.html?Id=${data.Id}`
+                window.location.href = `mainPage/main.html?Id=${data.Id}&Nickname=${document.querySelector("#name").value}`
                 break
             case 1:
                 let error = document.querySelector("#error") 
